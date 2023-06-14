a95e0f4dcdca6c12872b0dbbbe5dc7ec9a3661c1
Truong Nguyen <truong.nguyen@tymlez.com>
2023-06-14 11:03:58 +1000

    update ci

--- pgn-app/next.config.js
+++ pgn-app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+};
 
-module.exports = nextConfig
+if (process.env.NEXT_PUBLIC_BASE_PATH) {
+  nextConfig.basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+}
+
+module.exports = nextConfig;
